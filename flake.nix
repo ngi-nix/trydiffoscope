ed4cdb09395d3172559d329bd06fe4a6370584e5
Magic_RB <magic_rb@redalder.org>
2021-07-07 11:44:38 +0200

    Initial commit
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, self }@inputs:
+    let
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems' = systems: fun: nixpkgs.lib.genAttrs systems fun;
+      forAllSystems = forAllSystems' supportedSystems;
+    in
+    {
+      overlays.trydiffoscope = final: prev:
+        {
+          trydiffoscope = prev.callPackage ./trydiffoscope.nix {};
+        };
+
+      defaultPackage = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.trydiffoscope ]; };
+        in
+          pkgs.trydiffoscope
+      );
+    };
+}
