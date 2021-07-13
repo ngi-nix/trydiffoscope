2577db920024587b72a8bfb7852f27f5f36adcad
Magic_RB <magic_rb@redalder.org>
2021-07-13 14:12:10 +0200

    Fix missing trydiffoscope.nix, it just dissappeared
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- trydiffoscope.nix
+++ trydiffoscope.nix
@@ -1,1 +1,36 @@
+{ lib
+, fetchFromGitLab
+, python3Packages
+, which
+}:
+
+with python3Packages;
+
+let
+  version = "67.0.5";
+in
+buildPythonApplication {
+  pname = "trydiffoscope";
+  inherit version;
+
+  patchPhase =
+    let
+      escapePath = path: builtins.replaceStrings ["/"] ["\\/"] (toString path);
+    in
+      ''
+        ls
+        sed -i 's/\/usr\/bin\/env/'"$(which env | sed 's/\//\\\//gm')"'/' setup.py
+      '';
+
+  nativeBuildInputs = [ which ];
+
+  propagatedBuildInputs = [ requests ];
+
+  src = fetchFromGitLab {
+    domain = "salsa.debian.org";
+    owner = "reproducible-builds";
     repo = "trydiffoscope";
+    rev = "67.0.5";
+    sha256 = "sha256-tz2oHnZSxns5ol/qg+EBo1q08o5jm1lD7Tnyv5EMWts=";
+  };
+}
