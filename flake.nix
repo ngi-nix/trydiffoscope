8c60115ace84080d1b887525f59ae1034de64cd0
Magic_RB <magic_rb@redalder.org>
2021-07-23 11:06:41 +0200

    Add more stuff to flake.nix
    Signed-off-by: Magic_RB <magic_rb@redalder.org>

--- flake.nix
+++ flake.nix
@@ -9,21 +9,43 @@
       forAllSystems' = systems: fun: nixpkgs.lib.genAttrs systems fun;
       forAllSystems = forAllSystems' supportedSystems;
     in
-    {
-      overlays.trydiffoscope = final: prev:
-        {
-          trydiffoscope = prev.callPackage ./trydiffoscope.nix {};
-        };
+      with nixpkgs.lib;
+      {
+        overlays.trydiffoscope = final: prev:
+          {
+            trydiffoscope = prev.callPackage ./trydiffoscope.nix {};
+          };
 
-      defaultPackage = forAllSystems (system:
-        let
-          pkgs = import nixpkgs
-            { inherit system;
-              overlays = [ self.overlays.trydiffoscope ];
-              config.allowUnfree = true;
-            };
-        in
-          pkgs.trydiffoscope
-      );
-    };
+        overlay = self.overlays.trydiffoscope;
+
+        packages = forAllSystems (system:
+          { bitmask-vpn = self.defaultPackage.${system}; }
+        );
+
+        defaultPackage = forAllSystems (system:
+          let
+            pkgs = import nixpkgs
+              { inherit system;
+                overlays = [ self.overlays.trydiffoscope ];
+                config.allowUnfree = true;
+              };
+          in
+            pkgs.trydiffoscope
+        );
+
+        devShell = forAllSystems (system:
+          let
+            pkgs = import nixpkgs
+              { inherit system;
+                overlays = mapAttrsToList (_: id) self.overlays;
+              };
+          in
+            pkgs.mkShell {
+              nativeBuildInputs = with pkgs;
+                [ which
+                  (python3.withPackages (pkgs: with pkgs; [ requests ]))
+                ];
+            }
+        );
+      };
 }
